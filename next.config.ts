d3aab1110202af8490c96f22a44abf12b4cdf2d5
ashique-pallapay <ashique@pallapay.com>
2026-05-07 16:47:21 +0400

    fix

--- next.config.ts
+++ next.config.ts
@@ -1,10 +1,10 @@
-const withPWA = require('next-pwa')({
-  dest: 'public',
+const withPWA = require("next-pwa")({
+  dest: "public",
   register: true,
   skipWaiting: true,
+  disable: process.env.NODE_ENV === "development", // disable PWA in dev
 });
 
 module.exports = withPWA({
   reactStrictMode: true,
-  // Your other Next.js configs here
 });
